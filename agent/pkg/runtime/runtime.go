000f3b2bc35c54c5eb2986552f81b70d30997bcd
Alex Jones <alexsimonjones@gmail.com>
2022-05-22 22:15:07 +0100

    agent constructs in place

--- agent/pkg/runtime/runtime.go
+++ agent/pkg/runtime/runtime.go
@@ -10,4 +10,16 @@ import (
 
 func Start(syncr sync.ISync, server service.IService, ctx context.Context) {
 	log.Println("Starting run loop")
+
+	server.Serve(
+
+		func(ir service.IServiceRequest) service.IServiceResponse {
+
+			if ir.GetRequestType() == service.SERVICE_REQUEST_ALL_FLAGS {
+
+				return ir.GenerateServiceResponse("{}")
+			}
+			return nil
+		})
+
 }
