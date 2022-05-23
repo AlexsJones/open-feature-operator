797d86d78d8853bf373a84d721eab44e53d9dc8a
Alex Jones <alexsimonjones@gmail.com>
2022-05-23 07:32:34 +0100

    fixed broken deps

--- agent/pkg/runtime/runtime.go
+++ agent/pkg/runtime/runtime.go
@@ -2,16 +2,18 @@ package runtime
 
 import (
 	"context"
-	"log"
+	"time"
 
 	"github.com/open-feature/open-feature-operator/agent/pkg/service"
 	"github.com/open-feature/open-feature-operator/agent/pkg/sync"
 )
 
 func Start(syncr sync.ISync, server service.IService, ctx context.Context) {
-	log.Println("Starting run loop")
 
-	server.Serve(
+	// This is a very simple example of how the interface can be used for service and sync
+	// The service interface will serve requests whilst the sync interface is responsible
+	// for refreshing the configuration data
+	go server.Serve(
 
 		func(ir service.IServiceRequest) service.IServiceResponse {
 
@@ -22,4 +24,8 @@ func Start(syncr sync.ISync, server service.IService, ctx context.Context) {
 			return nil
 		})
 
+	for {
+
+		time.Sleep(time.Second * 10)
+	}
 }
