f35522cc311e6dfa5ffa33d6a27dd5ff99be45fd
Alex Jones <alexsimonjones@gmail.com>
2022-05-24 19:20:35 +0100

    reworked group

--- api/v1alpha1/groupversion_info.go
+++ api/v1alpha1/groupversion_info.go
@@ -16,7 +16,7 @@ limitations under the License.
 
 // Package v1alpha1 contains API Schema definitions for the config v1alpha1 API group
 //+kubebuilder:object:generate=true
-//+groupName=config.openfeature.dev
+//+groupName=core.openfeature.dev
 package v1alpha1
 
 import (
